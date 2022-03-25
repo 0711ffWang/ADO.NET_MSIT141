a9c1f13a4920b49f730401aa8487e98525758e02
0711ffWang <fionwang@iii.org.tw>
2022-03-25 15:49:44 +0800

    xxx

--- ADO.NET_MSIT141/Form1.cs
+++ ADO.NET_MSIT141/Form1.cs
@@ -16,31 +16,5 @@ namespace ADO.NET_MSIT141
         {
             InitializeComponent();
         }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("xxx");
-
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            //111111
-            //222222
-            //2222222222222222222222
-
-            //3333333333333333333333  333  33  33   33333
-            //3333333333333
-        }
     }
 }
